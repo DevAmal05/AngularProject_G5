d19a424fc1408e5bdf6ea8fe97cebc1ec7305934
DevAmal05 <62183621+DevAmal05@users.noreply.github.com>
2022-10-17 21:35:29 +0200

    init

--- helloworld/src/app/app-routing.module.ts
+++ helloworld/src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { Page404Component } from './page404/page404.component';
 import { TestComponent } from './test/test.component';
 
 const routes: Routes = [
-  {path:'test',component:TestComponent},
-  {path:'home',component:HomeComponent}
+  {path:'',component:TestComponent},
+  {path:'home',component:HomeComponent},
+
+
+
+
+
+
+
+
+
+
+  {path:'**',component:Page404Component}
 ];
 
 @NgModule({
