4ba7aad12c45529f1865e5b32da2b0f841f6c65f
DevAmal05 <62183621+DevAmal05@users.noreply.github.com>
2022-10-21 19:06:25 +0200

    init

--- helloworld/src/app/app.module.ts
+++ helloworld/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { HomeComponent } from './home/home.component';
 import { Page404Component } from './page404/page404.component';
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { Page404Component } from './page404/page404.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
